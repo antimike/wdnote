c805c25a2949f709e642aebc08cfff21c876424d
Vesdii <19791221+Vesdii@users.noreply.github.com>
2020-05-09 22:55:44 +0000

    Declare local variables at top of function

--- wdnote.plugin.zsh
+++ wdnote.plugin.zsh
@@ -9,16 +9,18 @@ cache="$ZSH_CACHE_DIR/wdnote-ignore"
 
 # Purge expired ignore files each month
 () {
-	local purge="$(cat $cache/next_purge)"
-	local now="$(date --utc +%s)"
+	local purge now files dir expire
+
+	purge="$(cat $cache/next_purge)"
+	now="$(date --utc +%s)"
 
 	[[ "$purge" -le "$now" ]] &&
 		print "$(date --utc --date="1 month" +%s)" >| "$cache/next_purge"
 
-	local files=($(ls --literal -1 --ignore='[^|]*' $cache))
+	files=($(ls --literal -1 --ignore='[^|]*' $cache))
 	for file in $files ; do
-		local dir="${file:gs/|/\/}"
-		local expire="$(cat "$cache/$file")"
+		dir="${file:gs/|/\/}"
+		expire="$(cat "$cache/$file")"
 
 		# Delete ignore file if expired or directory does not exist
 		[[ "$expire" -le "$now" || ! -d "$dir" ]] &&
@@ -32,13 +34,15 @@ unset cache
 autoload -Uz add-zsh-hook
 add-zsh-hook chpwd _chpwd_wdnote
 
-_chpwd_wdnote() {
+_chpwd_wdnote () {
+	local expire now wdnote
+
 	ZSH_WDNOTE_IGNORE="$ZSH_CACHE_DIR/wdnote-ignore/${PWD:gs/\//|}"
 
 	# Delete ignore file if expired
 	if [[ -f "$ZSH_WDNOTE_IGNORE" ]] ; then
-		local expire="$(cat "$ZSH_WDNOTE_IGNORE")"
-		local now="$(date --utc +%s)"
+		expire="$(cat "$ZSH_WDNOTE_IGNORE")"
+		now="$(date --utc +%s)"
 
 		[[ "$expire" -le "$now" ]] &&
 			rm "$ZSH_WDNOTE_IGNORE"
@@ -46,7 +50,7 @@ _chpwd_wdnote() {
 
 	# If not being ignored, print wdnote
 	if [[ ! -f "$ZSH_WDNOTE_IGNORE" ]] ; then
-		local wdnote="$PWD/.wdnote"
+		wdnote="$PWD/.wdnote"
 
 		[[ -f "$wdnote" ]] &&
 			cat "$wdnote"
@@ -55,7 +59,9 @@ _chpwd_wdnote() {
 _chpwd_wdnote
 
 # Permanently or temporarily ignore wdnote in current path
-wdnote() {
+wdnote () {
+	local expire now files
+
 	if [[ "$#" -eq "0" ]] ; then
 		cat ".wdnote"
 		return 0
@@ -76,11 +82,11 @@ wdnote() {
 		shift
 		# Indefinite ignore if no more parameters
 		if [[ "$#" -eq "0" ]] ; then
-			local expire="9999999999"
+			expire="9999999999"
 
 		else
 			# NOTE: BUG: not accepting +%s as part of command
-			local expire="$(date --utc --date="$@" +%s)"
+			expire="$(date --utc --date="$@" +%s)"
 			if [[ "$?" -ne "0" ]] ; then
 				print "wdnote: invalid ignore time '$@': see \33[4mman date\33[m"
 				return 1
@@ -88,7 +94,7 @@ wdnote() {
 		fi
 
 	elif [[ "$1" == "list" ]] ; then
-		local files="$(ls --literal -1 --ignore='[^|]*' "$ZSH_CACHE_DIR/wdnote-ignore")"
+		files="$(ls --literal -1 --ignore='[^|]*' "$ZSH_CACHE_DIR/wdnote-ignore")"
 		files="${files:gs/|/\/}"
 		print "$files"
 		return 0
@@ -98,7 +104,7 @@ wdnote() {
 		return 1
 	fi
 
-	local now="$(date --utc +%s)"
+	now="$(date --utc +%s)"
 
 	# Prevent redundancies
 	if [[ "$expire" -le "$now" ]] ; then
